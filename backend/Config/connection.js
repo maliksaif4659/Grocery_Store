6bdde32cfdb9a4e6bc69efdbe84247d14b77a684
maliksaif4659 <ms0360135@gmail.com>
2024-12-11 04:57:18 +0500

    Update connection.js

--- backend/Config/connection.js
+++ backend/Config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // Hardcoded MongoDB URI
-    const mongoURI = "mongodb+srv://safibro786:safi.4659@cluster0.4vw4j.mongodb.net/Store?retryWrites=true&w=majority&appName=Cluster0"; // Replace with your actual URI
+    const mongoURI = "mongodb+srv://malikshery09:malikshari098765@cluster0.svswbhp.mongodb.net/Grocery?retryWrites=true&w=majority&appName=Cluster0"; // Replace with your actual URI
     const conn = await mongoose.connect(mongoURI);
     console.log(`MongoDB Connected Successfully...!: ${conn.connection.host}`);
   } catch (error) {
